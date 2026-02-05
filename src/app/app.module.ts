2ad6584da83e6ec1e8b44bf1b331aaac2bd67db8
Adrian Joseph <adrianignatius15@hotmail.com>
2026-02-05 20:58:38 +0800

    Added imports of toolbar, button & the new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './pages/login.page/login.page.component';
 import { OrderPageComponent } from './pages/order.page/order.page.component';
 import { DocumentsPageComponent } from './pages/documents.page/documents.page.component';
-import { FooterComponent } from './components/footer/footer/footer.component';
-import { HeaderComponent } from './components/header/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +23,9 @@ import { HeaderComponent } from './components/header/header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
