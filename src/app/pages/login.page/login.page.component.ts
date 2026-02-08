8913886535b83d31fd77164b77ece4fc3437305a
Adrian Joseph <adrianignatius15@hotmail.com>
2026-02-08 17:31:54 +0800

    Completed the logic for login of the user

--- src/app/pages/login.page/login.page.component.ts
+++ src/app/pages/login.page/login.page.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../../services/authentication.service';
+import { Credentials } from '../../models/credentials/credentials';
 
 @Component({
   selector: 'app-login.page',
@@ -6,6 +9,33 @@ import { Component } from '@angular/core';
   templateUrl: './login.page.component.html',
   styleUrl: './login.page.component.css'
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit, OnDestroy {
+  public credentials: Credentials = {
+    username: "",
+    password: ""
+  };
 
+  constructor(
+    private routerSvc: Router,
+    private authenticationSvc: AuthenticationService
+  ) {}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  public login() {
+    this.authenticationSvc.login(this.credentials).subscribe((_creds) => {
+      this.authenticationSvc.saveLoginSession();
+      this.routerSvc.navigate(["/home"]);
+    });
+  }
+
+  public logout() {
+    this.authenticationSvc.logout();
+  }
 }
