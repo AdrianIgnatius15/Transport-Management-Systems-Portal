4f3922502376a8caaa5a4ba74389310b45d646b3
Adrian Joseph <adrianignatius15@hotmail.com>
2026-02-24 00:27:39 +0800

    Injected the new `UserProfileService` into the order page

--- src/app/pages/order.page/order.page.component.ts
+++ src/app/pages/order.page/order.page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserProfileService } from '../../services/user-profile.service';
 
 @Component({
   selector: 'app-order.page',
@@ -6,6 +7,10 @@ import { Component } from '@angular/core';
   templateUrl: './order.page.component.html',
   styleUrl: './order.page.component.css'
 })
-export class OrderPageComponent {
+export class OrderPageComponent implements OnInit {
+  constructor(private readonly userProfileSvc: UserProfileService) {}
 
+  async ngOnInit(): Promise<void> {
+    
+  }
 }
