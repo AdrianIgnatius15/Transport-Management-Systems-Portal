1c9443d692ba1b549ba51b2be72f9627266604bd
Adrian Joseph <adrianignatius15@hotmail.com>
2026-02-25 00:35:09 +0800

    Completed the service for user profile

--- src/app/services/user-profile.service.ts
+++ src/app/services/user-profile.service.ts
@@ -1,14 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import Keycloak, { KeycloakProfile } from 'keycloak-js';
+import { Client } from '../models/client';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserProfileService {
   
-  constructor(private readonly keycloak: Keycloak) {}
+  constructor(
+    private readonly keycloak: Keycloak,
+    private readonly httpClient: HttpClient,
+  ) {}
 
   public async getUserProfile(): Promise<KeycloakProfile | null> {
     return this.keycloak.authenticated ? await this.keycloak.loadUserProfile() : null;
+  }
+
+  public async upsertUserProfile(userProfile: KeycloakProfile) {
+    return await this.httpClient.post<Client>(`${import.meta.env.REST_API_BASE_URL}/api/client/upsert`, {
+      name: userProfile.firstName + " " + userProfile.lastName,
+      contactEmail: userProfile.email
+    });
+  }
+
+  public isUserAuthenticated() {
+    return this.keycloak.authenticated;
   }
 }
