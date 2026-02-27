25f2617ddeb5de46184b18a8c73daca1d1db5a95
Adrian Joseph <adrianignatius15@hotmail.com>
2026-02-27 17:08:14 +0800

    Injected `UserProfileDialog` & allowed user's profile update process

--- src/app/pages/order.page/order.page.component.ts
+++ src/app/pages/order.page/order.page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserProfileService } from '../../services/user-profile.service';
+import { MatDialog } from '@angular/material/dialog';
+import { UpdateUserProfileComponent } from '../../components/dialog/update-user-profile/update-user-profile.component';
+import { KeycloakProfile } from 'keycloak-js';
 
 @Component({
   selector: 'app-order.page',
@@ -11,10 +14,33 @@ export class OrderPageComponent implements OnInit {
   public userAuthenticatedFlag: boolean = false;
 
   constructor(
-    private readonly userProfileSvc: UserProfileService
+    private readonly userProfileSvc: UserProfileService,
+    private updateUserProfileDialog: MatDialog
   ) {}
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
     this.userAuthenticatedFlag = this.userProfileSvc.isUserAuthenticated();
+    if (this.userAuthenticatedFlag) {
+      let user = await this.userProfileSvc.getUserProfile();
+
+      if (user && user.email) {
+        this.userProfileSvc.getUserDetails(user.email)
+          .subscribe(client => {
+            if (client !== null && client.contactPhone.length === 0) {
+              const dialogRef = this.updateUserProfileDialog.open<UpdateUserProfileComponent, KeycloakProfile, boolean>(UpdateUserProfileComponent, {
+                height: "100%",
+                width: "400px",
+                data: user
+              });
+
+              dialogRef.afterClosed().subscribe(data => {
+                if (data === true) {
+                  this.userProfileSvc.logout();
+                }
+              });
+            }
+          });
+      }
+    }
   }
 }
