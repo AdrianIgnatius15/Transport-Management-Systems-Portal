69a1a4bbc3c37100fbb398c2e26d00600ba8dc9d
Adrian Joseph <adrianignatius15@hotmail.com>
2026-02-28 00:07:16 +0800

    Created a function to get all orders from the REST API

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,12 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Order } from '../models/order';
+import { catchError } from 'rxjs';
+import { ErrorHandlerService } from './error-handler.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
   
-  constructor(private httpClient: HttpClient) { }
+  constructor(
+    private httpClient: HttpClient,
+    private errorHandlerSvc: ErrorHandlerService
+  ) { }
 
-  
+  public getAllOrdersByEmail(email: string) {
+    return this.httpClient.get<Order[]>(`http://localhost:5181/api/client?email=${email}`)
+      .pipe(catchError(this.errorHandlerSvc.handlingError));
+  }
 }
