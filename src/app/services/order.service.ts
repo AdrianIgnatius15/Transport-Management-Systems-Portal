80bdf51f910f0c15154f3b149de007e957d52353
Adrian Joseph <adrianignatius15@hotmail.com>
2026-04-24 23:59:46 +0800

    Created new endpoint to get `order` list but with pagination

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -4,6 +4,8 @@ import { Order } from '../models/order';
 import { catchError } from 'rxjs';
 import { ErrorHandlerService } from './error-handler.service';
 import { Pagination } from '../models/request-body/pagination';
+import { PaginationOrderSearchParamters } from '../models/pagination/paginationOrderSearchParameters';
+import { PaginatedOrdersReadDto } from '../models/dtos/paginatedOrdersReadDto';
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +19,11 @@ export class OrderService {
 
   public getAllOrdersByEmail(email: string, pagination: Pagination) {
     return this.httpClient.post<Order[]>(`http://localhost:5230/api/order?email=${email}`, pagination)
+      .pipe(catchError(this.errorHandlerSvc.handlingError));
+  }
+
+  public getAllOrdersByShipperIdPaginated(shipperId: string, parameters: PaginationOrderSearchParamters) {
+    return this.httpClient.get<PaginatedOrdersReadDto>(`http://localhost:5230/api/order/all/shipperid?pageNumber=${parameters.pageNumber}&pageSize=${parameters.pageSize}&id=${shipperId}`)
       .pipe(catchError(this.errorHandlerSvc.handlingError));
   }
 }
