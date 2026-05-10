49f1b9d34c2714b1623216715f63688f9d8eb17f
Adrian Joseph <adrianignatius15@hotmail.com>
2026-05-10 22:42:41 +0800

    Created new service for location related features with API

---
+++ src/app/services/location.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ErrorHandlerService } from './error-handler.service';
+import { LocationLookup } from '../models/locations/location-lookup';
+import { LocationDetails } from '../models/locations/location-details';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LocationService {
+  constructor(
+    private readonly httpClient: HttpClient,
+    private readonly errorHandleSvc: ErrorHandlerService
+  ) {}
+
+  public getLatitudeAndLongitudeFromAddress(locationLookup: LocationLookup) {
+    return this.httpClient.get<LocationDetails>(`https://nominatim.openstreetmap.org/search?street=${locationLookup.street}&city=${locationLookup.city}&state=${locationLookup.state}&postalcode=${locationLookup.postalcode}&format=${locationLookup.format}`)
+      .pipe(this.errorHandleSvc.handlingError);
+  }
+}
