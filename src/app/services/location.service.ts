74cfd0df51563c30988329ba5589ec63062a9c6e
Adrian Joseph <adrianignatius15@hotmail.com>
2026-05-10 22:46:08 +0800

    Fixed error catching logic

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { ErrorHandlerService } from './error-handler.service';
 import { LocationLookup } from '../models/locations/location-lookup';
 import { LocationDetails } from '../models/locations/location-details';
+import { catchError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -14,7 +15,7 @@ export class LocationService {
   ) {}
 
   public getLatitudeAndLongitudeFromAddress(locationLookup: LocationLookup) {
-    return this.httpClient.get<LocationDetails>(`https://nominatim.openstreetmap.org/search?street=${locationLookup.street}&city=${locationLookup.city}&state=${locationLookup.state}&postalcode=${locationLookup.postalcode}&format=${locationLookup.format}`)
-      .pipe(this.errorHandleSvc.handlingError);
+    return this.httpClient.get<LocationDetails[]>(`https://nominatim.openstreetmap.org/search?street=${locationLookup.street}&city=${locationLookup.city}&state=${locationLookup.state}&postalcode=${locationLookup.postalcode}&format=${locationLookup.format}`)
+      .pipe(catchError(this.errorHandleSvc.handlingError));
   }
 }
