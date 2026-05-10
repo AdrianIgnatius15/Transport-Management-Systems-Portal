74d91f07767aa81d806a33a3731929462f2e5de9
Adrian Joseph <adrianignatius15@hotmail.com>
2026-05-10 22:42:25 +0800

    Created data models for location services

---
+++ src/app/models/locations/location-details.ts
@@ -0,0 +1,9 @@
+export class LocationDetails {
+    place_id: number = 0;
+    osm_id: number = 0;
+    type: string = "";
+    lat: string = "";
+    lon: string = "";
+    name: string = "";
+    display_name: string = "";
+}
---
+++ src/app/models/locations/location-lookup.ts
@@ -0,0 +1,7 @@
+export class LocationLookup {
+    street: string = "";
+    city?: string = "";
+    state?: string = "";
+    postalcode?: number = 0;
+    format: string = "json";
+}
