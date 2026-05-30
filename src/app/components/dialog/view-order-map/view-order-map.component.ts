c2527b303da87dcac8463c3eccf05817c4a0cac1
Adrian Joseph <adrianignatius15@hotmail.com>
2026-05-30 11:09:33 +0800

    Added the libraries & initial functions for loading map with the routes

--- src/app/components/dialog/view-order-map/view-order-map.component.ts
+++ src/app/components/dialog/view-order-map/view-order-map.component.ts
@@ -1,4 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit, signal, WritableSignal } from '@angular/core';
+import { LocationService } from '../../../services/location.service';
+import { Subject, takeUntil } from 'rxjs';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Order } from '../../../models/order';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import OSM from 'ol/source/OSM';
+import Feature from 'ol/Feature';
+import LineString from 'ol/geom/LineString';
+import Point from 'ol/geom/Point';
+import { fromLonLat, transformExtent } from 'ol/proj';
+import { Style, Stroke, Circle, Fill } from 'ol/style';
+import { boundingExtent } from 'ol/extent';
+import { LocationRoute } from '../../../models/locations/location-route';
 
 @Component({
   selector: 'app-view-order-map',
@@ -6,6 +23,28 @@ import { Component } from '@angular/core';
   templateUrl: './view-order-map.component.html',
   styleUrl: './view-order-map.component.css',
 })
-export class ViewOrderMapComponent {
+export class ViewOrderMapComponent implements OnInit, OnDestroy {
+  private destroyFlagSubject: Subject<void> = new Subject<void>();
 
+  public loadingMapAndRouteFlag: WritableSignal<boolean> = signal(false);
+  public locationRoute: LocationRoute = new LocationRoute();
+
+  constructor(
+    private readonly locationService: LocationService,
+    @Inject(MAT_DIALOG_DATA) public data: Order
+  ) {}
+
+  ngOnInit(): void {
+    this.locationService.getRoutesFromGraphhoper(this.data.shipmentAddress, this.data.deliveryAddress)
+        .pipe(takeUntil(this.destroyFlagSubject))
+      .subscribe(locationRoute => {
+        locationRoute ? this.loadingMapAndRouteFlag.set(true) : this.loadingMapAndRouteFlag.set(false);
+        this.locationRoute = JSON.parse(JSON.stringify(locationRoute));
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroyFlagSubject.next();
+    this.destroyFlagSubject.complete();
+  }
 }
